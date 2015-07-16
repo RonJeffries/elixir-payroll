7b3360bc07e24a7cc2babcc526aca76997cffae5
Ron Jeffries <ronjeffries@acm.org>
2015-07-16 12:45:28 -0400

    daily updates

--- ee-snip.exs
+++ ee-snip.exs
@@ -2,7 +2,17 @@ joe = %{name: "joe", rate: 10}
 sue = %{name: "sue", rate: 12}
 emps = %{1 => joe, 2 => sue}
 
-pay = fn(card) ->
+pay_stub = fn(card) -> 
   ee = emps[card.id]
-  [id: card.id, name: ee.name, base_pay: ee.rate*card.regular_hours]
+  [id: card.id, name: ee.name]
+end
+
+regular_pay = fn(pay_stub, card) ->
+  ee = emps[card.id]
+  [ regular_pay: ee.rate*card.regular_hours] ++ pay_stub
+end
+
+ot1_pay = fn(pay_stub, card) ->
+  ee = emps[card.id]
+  [ ot1_pay: ee.rate*card.ot1_hours] ++ pay_stub
 end
--- payroll.exs
+++ payroll.exs
@@ -13,4 +13,39 @@ defmodule Pay do
       %Timecard{id: 2, regular_hours: 40, ot1_hours: 10, ot2_hours: 5}
     ]
   end
+
+  @joe  %{name: "joe", rate: 10}
+  @sue  %{name: "sue", rate: 12}
+  @emps %{1 => @joe, 2 => @sue}
+
+  def pay_timecard(card) do
+    ee = @emps[card.id]
+
+    pay_stub = [id: card.id, name: ee.name]
+
+    regular_pay = fn(pay_stub) ->
+      [ regular_pay: ee.rate*card.regular_hours ] ++ pay_stub
+    end
+
+    ot1_pay = fn(pay_stub) ->
+      [ ot1_pay: ee.rate*card.ot1_hours] ++ pay_stub
+    end
+
+    ot2_pay = fn(pay_stub) ->
+      [ ot2_pay: ee.rate*card.ot2_hours] ++ pay_stub
+    end
+
+# this guy can see card and ee and does not need them
+
+    base_pay = fn(pay_stub) ->
+      [ base_pay: pay_stub[:regular_pay] + pay_stub[:ot1_pay] + pay_stub[:ot2_pay]]  ++ pay_stub
+    end
+
+    pay_stub 
+      |> regular_pay.() 
+      |> ot1_pay.()
+      |> ot2_pay.()
+      |> base_pay.()
+  end
+
 end
